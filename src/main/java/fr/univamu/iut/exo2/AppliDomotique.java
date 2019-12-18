ed6a6ac8105e6e58fd07284b64858b7aac01383f
Adem <louriachi.adem@gmail.com>
2019-12-18 09:34:53 +0100

    il y a beaucoup trop d'erreurs pour le moment mais ça va s'arranger

--- src/main/java/fr/univamu/iut/exo2/AppliDomotique.java
+++ src/main/java/fr/univamu/iut/exo2/AppliDomotique.java
@@ -1,7 +1,42 @@
 package fr.univamu.iut.exo2;
 
-public class AppliDomotique {
-    public static void main(String[] args) {
-        throw new RuntimeException("Not yet implemented !");
+import fr.univamu.iut.exo1.Connectable;
+import sun.tools.java.ClassNotFound;
+
+import javax.imageio.IIOException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.String;
+
+public class  AppliDomotique {
+    public static String main(String[] args) {
+        abstract class FabAbsConnectable {
+            public Connectable fabriquer(String s)throws ClassNotFound, ClassCastException{
+                Connectable c = creer(s);
+                //c.equiper(c);
+                return c;
+            }
+            public abstract Connectable creer (String s)throws ClassCastException, ClassNotFound; //faire un switch
+            public abstract void equiper (Connectable c);
+        }
+        public static String menu(){
+            String nomClasse = new String();
+            System.out.println("fin");
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            try {
+                nomClasse = in.readLine();
+            }
+            catch(IOException e) {}
+            return nomClasse;
+        }
+        public static void connecter (String nomClasse) {
+            try {
+                Connectable c = fab.fabriquer(nomClasse);
+            objets.add(c);
+            System.out.println(c);
+            }
+            catch (ClassNotFound e) {}
     }
 }
